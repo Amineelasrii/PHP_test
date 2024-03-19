104a37b5c20776ab5c33481e79274a62c2a3dd51
Amineelasrii <amineelasri235@gmail.com>
2024-03-19 21:14:03 +0000

    PHP_test

---
+++ exercice01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $nbr1 = 655;
+    $nbr2 = 657;
+    $somme = $nbr1 + $nbr2;
+    echo $somme;
+    echo "<br>";
+    echo gettype($somme);
+?>
+
+</body>
+</html>
