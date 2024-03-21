5d4f6b40a7ed752e4ed9354b1759627e9f3733e7
Amineelasrii <amineelasri235@gmail.com>
2024-03-21 20:12:35 +0000

    PHP_test

---
+++ exercice02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+//exemple de str_word_count() 
+$str = "c'est quoi un serveur";
+echo "<br>";
+echo str_word_count($str); // Output: 2
+
+?>
+
+</body>
+</html>
